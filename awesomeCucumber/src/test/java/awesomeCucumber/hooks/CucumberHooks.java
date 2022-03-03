8677fe05983256557bba719e6827124ca93dbe3f
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-03 06:56:52 +0530

    Added before and after hooks

--- awesomeCucumber/src/test/java/awesomeCucumber/hooks/CucumberHooks.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/hooks/CucumberHooks.java
@@ -1,16 +1,28 @@
 package awesomeCucumber.hooks;
 
+import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 
 public class CucumberHooks {
     @BeforeAll
-    public static void beforeAll(){
+    public static void beforeAll() {
         System.out.println("Before All");
     }
 
     @AfterAll
-    public static void afterAll(){
+    public static void afterAll() {
         System.out.println("After All");
+    }
+
+    @Before
+    public static void before() {
+        System.out.println("I'm from Before hook");
+    }
+
+    @After
+    public static void after() {
+        System.out.println("I'm from After hook");
     }
 }
