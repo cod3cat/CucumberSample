99c0e814c1987457478b07dd358ae33eef059530
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-05 12:50:13 +0530

    Created diver factory to get a static instance and initialize it in one place

---
+++ awesomeCucumber/src/test/java/awesomeCucumber/factory/DriverFactory.java
@@ -0,0 +1,19 @@
+package awesomeCucumber.factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver initializeDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
