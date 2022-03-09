c0b19ec7b31de15f8850f9a47d389137ded3b5d7
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-09 07:02:51 +0530

    Updated setBillingDetails to accept Billing Details domain object instead of data from the step

--- awesomeCucumber/src/test/java/awesomeCucumber/pages/CheckoutPage.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/pages/CheckoutPage.java
@@ -1,5 +1,6 @@
 package awesomeCucumber.pages;
 
+import awesomeCucumber.domainObjects.BillingDetails;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,68 +20,65 @@ public class CheckoutPage extends BasePage{
     @FindBy(id = "billing_state") private WebElement billingStateDropdown;
     @FindBy(id = "billing_postcode") private WebElement billingZipField;
     @FindBy(id = "billing_email") private WebElement billingEmailAddress;
-
     @FindBy(id = "place_order") private WebElement placeOrderButton;
     @FindBy(css = ".woocommerce-notice") private WebElement noticeText;
 
     public CheckoutPage enterBillingFirstName(String billingFirstName) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingFirstNameField));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingFirstNameField));
         e.clear();
         e.sendKeys(billingFirstName);
         return  this;
     }
 
     public CheckoutPage enterBillingLastName(String billingLastName) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingLastNameField));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingLastNameField));
         e.clear();
         e.sendKeys(billingLastName);
         return  this;
     }
 
     public CheckoutPage enterBillingAddressOne(String billingAddressOne) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingAddressOneField));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingAddressOneField));
         e.clear();
         e.sendKeys(billingAddressOne);
         return  this;
     }
 
     public CheckoutPage enterBillingCity(String billingCity) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingCityField));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingCityField));
         e.clear();
         e.sendKeys(billingCity);
         return  this;
     }
 
     public CheckoutPage selectBillingState(String billingState) {
-        Select select = new Select(wait.until(ExpectedConditions.visibilityOf(billingStateDropdown)));
+        Select select = new Select(wait.until(ExpectedConditions.elementToBeClickable(billingStateDropdown)));
         select.selectByVisibleText(billingState);
         return this;
     }
 
     public CheckoutPage enterBillingZipCode(String zipCode) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingZipField));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingZipField));
         e.clear();
         e.sendKeys(zipCode);
         return  this;
     }
 
     public CheckoutPage enterBillingEmail(String billingEmail) {
-        WebElement e = wait.until(ExpectedConditions.visibilityOf(billingEmailAddress));
+        WebElement e = wait.until(ExpectedConditions.elementToBeClickable(billingEmailAddress));
         e.clear();
         e.sendKeys(billingEmail);
         return  this;
     }
 
-    public CheckoutPage setBillingDetails(String billingFirstName, String billingLastName, String billingAddressOne,
-                                          String billingCity, String billingStateName, String billingZip,
-                                          String billingEmail) {
-       return enterBillingFirstName(billingFirstName).
-               enterBillingLastName(billingLastName).
-               enterBillingAddressOne(billingAddressOne).
-               enterBillingCity(billingCity).
-               selectBillingState(billingStateName).
-               enterBillingZipCode(billingZip).
-               enterBillingEmail(billingEmail);
+    public CheckoutPage setBillingDetails(BillingDetails billingDetails) {
+       return enterBillingFirstName(billingDetails.getBillingFirstNameField()).
+               enterBillingLastName(billingDetails.getBillingLastNameField()).
+               enterBillingAddressOne(billingDetails.getBillingAddressOneField()).
+               enterBillingCity(billingDetails.getBillingCityField()).
+               selectBillingState(billingDetails.getBillingStateDropdown()).
+               enterBillingZipCode(billingDetails.getBillingZipField()).
+               enterBillingEmail(billingDetails.getBillingEmailAddress());
     }
 
     public CheckoutPage placeOrder() {
