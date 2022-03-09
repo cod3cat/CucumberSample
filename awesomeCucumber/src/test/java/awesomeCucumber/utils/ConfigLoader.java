f0c8450347e62f35ceea6d16c73ea98d2bc49a33
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-09 08:59:03 +0530

    Created config loader to load and share configs

---
+++ awesomeCucumber/src/test/java/awesomeCucumber/utils/ConfigLoader.java
@@ -0,0 +1,26 @@
+package awesomeCucumber.utils;
+
+import java.util.Properties;
+
+public class ConfigLoader {
+    private final Properties properties;
+    private static ConfigLoader configLoader;
+
+    private ConfigLoader() {
+        properties = PropertyUtils.propertyLoader("src/test/resources/config.properties");
+    }
+
+    public static ConfigLoader getInstance() {
+        if(configLoader == null) {
+            configLoader = new ConfigLoader();
+        }
+
+        return configLoader;
+    }
+
+    public String getBaseUrl() {
+        String prop = properties.getProperty("baseUrl");
+        if(prop != null) return prop;
+        else throw new RuntimeException("property 'baseUrl' is not configured in the config.properties file");
+    }
+}
