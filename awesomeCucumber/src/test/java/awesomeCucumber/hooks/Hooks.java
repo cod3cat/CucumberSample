51725018b58d5471965c274115d97b9818188134
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-10 23:02:22 +0530

    Added code to print scenario name and the thread running it

--- awesomeCucumber/src/test/java/awesomeCucumber/hooks/Hooks.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/hooks/Hooks.java
@@ -3,6 +3,7 @@ package awesomeCucumber.hooks;
 import awesomeCucumber.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
 
 public class Hooks {
@@ -10,12 +11,14 @@ public class Hooks {
     private WebDriver driver;
 
     @Before
-    public void before() {
-        driver = DriverFactory.initializeDriver();
+    public void before(Scenario scenario) {
+        System.out.println("Before - Thread ID: " + Thread.currentThread().getId() + " .Scneario Name : " + scenario.getName());
+        driver = DriverFactory.initializeDriver(System.getProperty("browser", "chrome"));
     }
 
     @After
-    public void after(){
+    public void after(Scenario scenario){
+        System.out.println("After - Thread ID: " + Thread.currentThread().getId() + " .Scneario Name : " + scenario.getName());
         driver.quit();
     }
 }
