2ebcb30f08b721cec19ab35630ff3f923053d59e
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-10 23:08:26 +0530

    Driver factory now supports firefox and selenium Driver factory now uses threadlocal to create a thread based driver object

--- awesomeCucumber/src/test/java/awesomeCucumber/factory/DriverFactory.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/factory/DriverFactory.java
@@ -2,18 +2,34 @@ package awesomeCucumber.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    private static WebDriver driver;
+    private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
-    public static WebDriver initializeDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+
+    public static WebDriver initializeDriver(String browser) {
+        WebDriver driver;
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new IllegalStateException("Invalid Browser" + browser);
+        }
+        DriverFactory.driverThreadLocal.set(driver);
         driver.manage().window().maximize();
         return driver;
     }
 
     public static WebDriver getDriver() {
-        return driver;
+        return driverThreadLocal.get();
     }
 }
