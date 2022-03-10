e86553ef5cc37a6e5118f87ef6165ba2dc683d41
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-10 23:05:34 +0530

    Updated testNG runner to support parallel execution. #Note - If running tests using junit runner and mvn, then disable testng dependency in pom and comment testng based code from the script

--- awesomeCucumber/src/test/java/awesomeCucumber/runners/TestNGRunner.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/runners/TestNGRunner.java
@@ -1,16 +1,21 @@
+/*
 package awesomeCucumber.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber.html"},
         features = "src/test/resources/awesomeCucumber",
-        glue = "awesomeCucumber.stepDefs",
+        glue = "awesomeCucumber",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        tags = "@scenario2"
+        tags = "@smoke"
 )
 public class TestNGRunnerTest extends AbstractTestNGCucumberTests {
-}
-
-
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}*/
