785aadd6f7d3e1db1156a941feb0ba6bc2433077
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-12 10:46:22 +0530

    Updated hooks class to test context and set driver context

--- awesomeCucumber/src/test/java/awesomeCucumber/hooks/Hooks.java
+++ awesomeCucumber/src/test/java/awesomeCucumber/hooks/Hooks.java
@@ -1,5 +1,6 @@
 package awesomeCucumber.hooks;
 
+import awesomeCucumber.context.TestContext;
 import awesomeCucumber.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -9,16 +10,22 @@ import org.openqa.selenium.WebDriver;
 public class Hooks {
 
     private WebDriver driver;
+    private final TestContext testContext;
+
+    public Hooks(TestContext context) {
+        testContext = context;
+    }
 
     @Before
     public void before(Scenario scenario) {
         System.out.println("Before - Thread ID: " + Thread.currentThread().getId() + " .Scneario Name : " + scenario.getName());
         driver = DriverFactory.initializeDriver(System.getProperty("browser", "chrome"));
+        testContext.driver = driver;
     }
 
     @After
     public void after(Scenario scenario){
-        System.out.println("After - Thread ID: " + Thread.currentThread().getId() + " .Scneario Name : " + scenario.getName());
+        System.out.println("After    - Thread ID: " + Thread.currentThread().getId() + " .Scneario Name : " + scenario.getName());
         driver.quit();
     }
 }
