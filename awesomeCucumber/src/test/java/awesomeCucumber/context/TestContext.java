cd89c6de1b88cd820ddfc48601911d4c6eea9683
Praveen G <25690316+cod3cat@users.noreply.github.com>
2022-03-12 10:44:41 +0530

    Used pico container to create a test context and share between the tests/steps

---
+++ awesomeCucumber/src/test/java/awesomeCucumber/context/TestContext.java
@@ -0,0 +1,9 @@
+package awesomeCucumber.context;
+
+import awesomeCucumber.domainObjects.BillingDetails;
+import org.openqa.selenium.WebDriver;
+
+public class TestContext {
+    public WebDriver driver;
+    public BillingDetails billingDetails;
+}
